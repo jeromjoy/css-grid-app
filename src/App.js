1e37a8d7cc69da2f5f6364aa9c49768e48b8595c
Jerom Joy Vattakeril <jeromjoy@users.noreply.github.com>
2019-07-07 22:15:34 +0100

    added grid layout for mobile view

--- src/App.js
+++ src/App.js
@@ -5,12 +5,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="App-header-with-logo"></div>
+      <div className="App-header-with-logo">
+      <h1>Event app</h1></div>
       <div className="App-banner">
       <h3>This is a app banner</h3></div>
-      <div className="App-event-list"></div>
-      <div className="App-sponsored-events"></div>
-      <div className="App-location"></div>
+      <div className="App-event-list">
+      {[...Array(10).keys()].map( item=> (
+        <li>Event {item + 1}</li>
+      ))}
+      </div>
+      <div className="App-sponsored-events">
+      {[...Array(3).keys()].map( item=> (
+        <li>Sponsored Event {item + 1}</li>
+      ))}
+      </div>
+      <div className="App-location">
+      123 Avenue road, Big City, Large county, Country </div>
     </div>
   );
 }
